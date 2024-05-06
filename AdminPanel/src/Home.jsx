6408167dc8c043cacfc23166cb697bbc30109055
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-05-06 13:00:43 +0300

    forth commit

--- AdminPanel/src/Home.jsx
+++ AdminPanel/src/Home.jsx
@@ -63,21 +63,21 @@ function Home() {
         <div className='main-cards'>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>PRODUCTS</h3>
+                    <h3>MEMBERS</h3>
                     <BsFillArchiveFill className='card_icon'/>
                 </div>
-                <h1>300</h1>
+                <h1>320</h1>
             </div>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>CATEGORIES</h3>
+                    <h3>CLERGY STAFF</h3>
                     <BsFillGrid3X3GapFill className='card_icon'/>
                 </div>
                 <h1>12</h1>
             </div>
             <div className='card'>
                 <div className='card-inner'>
-                    <h3>CUSTOMERS</h3>
+                    <h3>SERMONS</h3>
                     <BsPeopleFill className='card_icon'/>
                 </div>
                 <h1>33</h1>
