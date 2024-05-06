790dcc2f504396bd00a9050bbf6d3a5dcf8711c5
Mark Kimathi <mark.kimathi@technobraingroup.com>
2024-05-06 12:54:52 +0300

    third commit

--- AdminPanel/src/App.jsx
+++ AdminPanel/src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import Home from './Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
 
   return (
-    <>
-  
-    </>
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
   )
 }
 
